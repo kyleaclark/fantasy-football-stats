5c2418d62284e4e7e57d8a1965166623bbb39bf3
Kyle Clark <kyle.andrew.clark@gmail.com>
2016-04-17 21:17:51 -0700

    Updated csv data with latest 2015 season fantasy stats; fixed bug that caused exception when player did not participate in a segment of the season; other cleanup and added todo comments

--- src/core_stats.py
+++ src/core_stats.py
@@ -9,7 +9,7 @@ def generate_position_stats(plyr_list, config):
     stats = query.aggregrate_position_stats(config)
 
     for plyr in stats:
-        plyr_list.append(generate_plyr_obj(plyr, config))
+        plyr_list.append(generate_plyr_info(plyr, config))
 
     advanced_stats.calc_position_stats(plyr_list, config['sample_size'], config['sample_trim'])
 
@@ -22,28 +22,42 @@ def calc_plyr_weeks(games):
 
     return weeks
 
-def generate_plyr_obj(plyr, config):
+def generate_plyr_info(plyr, config):
     'Generates a player object and associated core stats'
     plyr_id = plyr.player_id
 
     # query player game weeks
     config['weeks'] = util.fantasy_full_weeks()
     full_games = query.aggregate_plyr_games(plyr_id, config)
+    plyr_full_aggregate = query.aggregate_plyr_stats(plyr_id, config)
+    if plyr_full_aggregate:
+        plyr_full_dict = config['generate_plyr_stats_dict'](plyr_full_aggregate[0])
+    else:
+        plyr_full_dict = config['generate_plyr_default_dict']()
 
     # query player stats (weeks 1-8)
     config['weeks'] = util.fantasy_beg_weeks()
     beg_games = query.aggregate_plyr_games(plyr_id, config)
-    plyr_beg_stats = query.aggregate_plyr_stats(plyr_id, config)[0]
+    plyr_beg_aggregate = query.aggregate_plyr_stats(plyr_id, config)
+    if plyr_beg_aggregate:
+        plyr_beg_dict = config['generate_plyr_stats_dict'](plyr_beg_aggregate[0])
+    else:
+        plyr_beg_dict = config['generate_plyr_default_dict']()
 
     # query player stats (weeks 8-16)
     config['weeks'] = util.fantasy_end_weeks()
     end_games = query.aggregate_plyr_games(plyr_id, config)
-    plyr_end_stats = query.aggregate_plyr_stats(plyr_id, config)[0]
+    plyr_end_aggregate = query.aggregate_plyr_stats(plyr_id, config)
+    if plyr_end_aggregate:
+        plyr_end_dict = config['generate_plyr_stats_dict'](plyr_end_aggregate[0])
+    else:
+        plyr_end_dict = config['generate_plyr_default_dict']()
 
     # calculate player fantasy points
-    full_pts = config['calc_fantasy_stats'](plyr)
-    beg_pts = config['calc_fantasy_stats'](plyr_beg_stats)
-    end_pts = config['calc_fantasy_stats'](plyr_end_stats)
+    # TODO: better naming convention than config to indicate dynamic methods in object
+    full_pts = config['calc_fantasy_stats'](plyr_full_dict)
+    beg_pts = config['calc_fantasy_stats'](plyr_beg_dict)
+    end_pts = config['calc_fantasy_stats'](plyr_end_dict)
 
     # total weeks, 1-8 weeks, 9-16 weeks played
     full_weeks = calc_plyr_weeks(full_games)
@@ -53,10 +67,10 @@ def generate_plyr_obj(plyr, config):
     end_weeks = calc_plyr_weeks(end_games)
     end_weeks_played = len(end_weeks)
 
-    full_avg_pts = full_pts / full_weeks_played
-    beg_avg_pts = beg_pts / beg_weeks_played
-    end_avg_pts = end_pts / end_weeks_played
-    availability = (full_weeks_played / 15)
+    full_avg_pts = (full_pts / full_weeks_played) if full_pts > 0 else 0
+    beg_avg_pts = (beg_pts / beg_weeks_played) if beg_pts > 0 else 0
+    end_avg_pts = (end_pts / end_weeks_played) if end_pts > 0 else 0
+    availability = (full_weeks_played / 15) if full_weeks_played > 0 else 0
 
     print 'Name: %s, Pts %d %.1f' % (plyr.player, full_pts, full_avg_pts)
 

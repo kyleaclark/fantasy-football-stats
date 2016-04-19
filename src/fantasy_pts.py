9db9d1cb6a039ae2217fd2f9f380571b163d8864
Kyle Clark <kyle.andrew.clark@gmail.com>
2016-04-18 20:35:57 -0700

    Updated qb, pos plyr fantasy stats calculation to be dynamic and not rely on static configurations; decreased code footprint in stats calculation

--- src/fantasy_pts.py
+++ src/fantasy_pts.py
@@ -1,17 +1,35 @@
 import stat_utils as util
 
+# TODO: Centralize qb stat categories in a separate module
+qb_plyr_categories = [
+    'rushing_yds',
+    'rushing_tds',
+    'passing_yds',
+    'passing_tds',
+    'passing_int',
+    'fumbles_lost'
+]
+
+# TODO: Centralize qb stat categories in a separate module
+pos_plyr_categories = [
+    'rushing_yds',
+    'rushing_tds',
+    'receiving_yds',
+    'receiving_tds',
+    'fumbles_lost'
+]
+
+qb_plyr_stat_methods = util.qb_plyr_methods()
+pos_plyr_stat_methods = util.pos_plyr_methods()
+
 def calc_qb(plyr_dict):
     'Calculates fantasy points by a qb'
 
-    rushing_yds_pts = util.calc_reg_yd_pts(plyr_dict['rushing_yds'])
-    rushing_tds_pts = util.calc_reg_td_pts(plyr_dict['rushing_tds'])
-    passing_yds_pts = util.calc_pass_yd_pts(plyr_dict['passing_yds'])
-    passing_tds_pts = util.calc_pass_td_pts(plyr_dict['passing_tds'])
-    passing_int_pts = util.calc_int_pts_loss(plyr_dict['passing_int'])
-    fumbles_lost_pts = util.calc_fum_pts_loss(plyr_dict['fumbles_lost'])
+    qb_plyr_pts = {category:qb_plyr_stat_methods[category](stat) for category, stat in plyr_dict.iteritems()}
 
-    return rushing_yds_pts + rushing_tds_pts + passing_yds_pts + passing_tds_pts + fumbles_lost_pts + passing_int_pts
+    return sum(qb_plyr_pts.itervalues())
 
+# TODO: consolidate all of these dict keys to be dynamic (possibly move into separate module)
 def generate_qb_plyr_stats_dict(plyr_stats):
     return {
         'rushing_yds': plyr_stats.rushing_yds or 0,
@@ -23,27 +41,16 @@ def generate_qb_plyr_stats_dict(plyr_stats):
     }
 
 def generate_qb_plyr_default_dict():
-    return {
-        'rushing_yds': 0,
-        'rushing_tds': 0,
-        'passing_yds': 0,
-        'passing_tds': 0,
-        'passing_int': 0,
-        'fumbles_lost': 0
-    }
+    return {category:0 for category in qb_plyr_categories}
 
 def calc_pos_plyr(plyr_dict):
     'Calculates fantasy points by a rb, wr, or te'
 
-    rushing_yds_pts = util.calc_reg_yd_pts(plyr_dict['rushing_yds'])
-    rushing_tds_pts = util.calc_reg_td_pts(plyr_dict['rushing_tds'])
-    receiving_yds_pts = util.calc_reg_yd_pts(plyr_dict['receiving_yds'])
-    receiving_tds_pts = util.calc_reg_td_pts(plyr_dict['receiving_tds'])
-    fumbles_lost_pts = util.calc_fum_pts_loss(plyr_dict['fumbles_lost'])
+    pos_plyr_pts = {category:pos_plyr_stat_methods[category](stat) for category, stat in plyr_dict.iteritems()}
 
-    return rushing_yds_pts + rushing_tds_pts + receiving_yds_pts + receiving_tds_pts + fumbles_lost_pts
+    return sum(pos_plyr_pts.itervalues())
 
-# TODO: consolidate all of these dict keys to be dynamic
+# TODO: consolidate all of these dict keys to be dynamic (possibly move into separate module)
 def generate_pos_plyr_stats_dict(plyr_stats):
     return {
         'rushing_yds': plyr_stats.rushing_yds or 0,
@@ -54,10 +61,4 @@ def generate_pos_plyr_stats_dict(plyr_stats):
     }
 
 def generate_pos_plyr_default_dict():
-    return {
-        'rushing_yds': 0,
-        'rushing_tds': 0,
-        'receiving_yds': 0,
-        'receiving_tds': 0,
-        'fumbles_lost': 0
-    }
+    return {category:0 for category in pos_plyr_categories}
--- src/stat_utils.py
+++ src/stat_utils.py
@@ -39,3 +39,24 @@ def fantasy_beg_weeks():
 def fantasy_end_weeks():
     'Returns list of end fantasy weeks 9-16'
     return end_weeks
+
+# TODO: how to make the object exportable outside of method
+def qb_plyr_methods():
+    return {
+        'rushing_yds': calc_reg_yd_pts,
+        'rushing_tds': calc_reg_td_pts,
+        'passing_yds': calc_pass_yd_pts,
+        'passing_tds': calc_pass_td_pts,
+        'passing_int': calc_int_pts_loss,
+        'fumbles_lost': calc_fum_pts_loss
+    }
+
+# TODO: how to make the object exportable outside of method
+def pos_plyr_methods():
+    return {
+        'rushing_yds': calc_reg_yd_pts,
+        'rushing_tds': calc_reg_td_pts,
+        'receiving_yds': calc_reg_yd_pts,
+        'receiving_tds': calc_reg_td_pts,
+        'fumbles_lost': calc_fum_pts_loss
+    }
